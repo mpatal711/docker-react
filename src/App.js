5771a6ea6593d5ddcf5c282ed6555ae864a450a9
mpatal711 <194726000+mpatal711@users.noreply.github.com>
2025-03-07 09:33:05 -0500

    fix branch name and test a change to trigger

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React cool!
+          Learn React Github actions!
         </a>
       </header>
     </div>
