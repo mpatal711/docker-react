ae1b1ef80d15efac3a1435dede83acc9ec29ed1b
mpatal711 <194726000+mpatal711@users.noreply.github.com>
2025-03-30 19:27:48 -0400

    edit: minor update fix unit test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Minor update!
+          learn react Minor update!
         </a>
       </header>
     </div>
